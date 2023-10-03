1ceeeda29741b1ed7c313f9d771e868e85d030f3
Wilson Tian <wilson.tian@perkinelmer.com>
2023-10-03 15:45:19 +0800

    ORB doesn't work #11

---
+++ src/types/opencv/ORB.ts
@@ -0,0 +1,22 @@
+import { Feature2D, float, int } from "./_types";
+
+/**
+ * https://docs.opencv.org/4.8.0/db/d95/classcv_1_1ORB.html
+ */
+export declare class ORB extends Feature2D {
+  public constructor(
+    nfeatures?: int,
+    scaleFactor?: float,
+    nlevels?: int,
+    edgeThreshold?: int,
+    firstLevel?: int,
+    WTA_K?: int,
+    scoreType?: ORBScoreType,
+    patchSize?: int,
+    fastThreshold?: int,
+  );
+}
+
+type ORBScoreType = int;
+export declare const ORB_HARRIS_SCORE: ORBScoreType;
+export declare const ORB_FAST_SCORE: ORBScoreType;
