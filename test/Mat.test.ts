12147749867a5d02aad61079eb67863fd4fe3cb7
Wilson Tian <wilson.tian@perkinelmer.com>
2023-11-06 14:52:13 +0800

    make test can run

--- test/Mat.test.ts
+++ test/Mat.test.ts
@@ -1,38 +1,61 @@
-import * as cv from "../src";
+import "../src";
+
+beforeAll(async () => {
+  return new Promise((resolve) => {
+    const _cv = require("../dist/opencv");
+    _cv.onRuntimeInitialized = resolve;
+    global.cv = _cv;
+  });
+});
 
 describe("Mat", () => {
   it("shoud pass TS type validations", () => {
-    const img = new cv.Mat();
+    try {
+      const img = new cv.Mat();
 
-    const imgGray = new cv.Mat();
-    cv.cvtColor(img, imgGray, cv.COLOR_RGBA2GRAY);
+      const imgGray = new cv.Mat();
+      cv.cvtColor(img, imgGray, cv.COLOR_RGBA2GRAY);
 
-    const imgBlur = new cv.Mat();
-    cv.GaussianBlur(
-      imgGray,
-      imgBlur,
-      new cv.Size(5, 5),
-      0,
-      0,
-      cv.BORDER_DEFAULT,
-    );
+      const imgBlur = new cv.Mat();
+      cv.GaussianBlur(
+        imgGray,
+        imgBlur,
+        new cv.Size(5, 5),
+        0,
+        0,
+        cv.BORDER_DEFAULT,
+      );
 
-    const imgThresh = new cv.Mat();
-    cv.threshold(imgBlur, imgThresh, 0, 255, cv.THRESH_BINARY + cv.THRESH_OTSU);
+      const imgThresh = new cv.Mat();
+      cv.threshold(
+        imgBlur,
+        imgThresh,
+        0,
+        255,
+        cv.THRESH_BINARY + cv.THRESH_OTSU,
+      );
 
-    const contours = new cv.MatVector();
-    const hierarchy = new cv.Mat();
+      const contours = new cv.MatVector();
+      const hierarchy = new cv.Mat();
 
-    cv.findContours(
-      imgThresh,
-      contours,
-      hierarchy,
-      cv.RETR_CCOMP,
-      cv.CHAIN_APPROX_SIMPLE,
-    );
+      cv.findContours(
+        imgThresh,
+        contours,
+        hierarchy,
+        cv.RETR_CCOMP,
+        cv.CHAIN_APPROX_SIMPLE,
+      );
 
-    const channels = new cv.MatVector();
-    cv.split(img, channels);
-    cv.merge(channels, img);
+      const channels = new cv.MatVector();
+      cv.split(img, channels);
+      cv.merge(channels, img);
+    } catch (err) {
+      if (typeof err === "number") {
+        const error = cv.exceptionFromPtr(err);
+        throw error;
+      } else {
+        throw err;
+      }
+    }
   });
 });
